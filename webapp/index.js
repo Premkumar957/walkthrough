027991df047eee9d56e8acf75f9bf21d9e271c9b
Premkumar <premkumar05604@gmail.com>
2025-10-17 13:09:40 +0530

    Fourth commit

--- webapp/index.js
+++ webapp/index.js
@@ -1,9 +1,18 @@
 sap.ui.define([
-	"sap/m/Text"
-], (Text) => {
+	"sap/m/Text",
+	"sap/m/Button",
+	"sap/m/MessageToast"
+], (Text, Button, MessageToast) => {
 	"use strict";
 
 	new Text({
-		text: "Hello World"
+		text: "Become the programmer you are meant to be!"
 	}).placeAt("content"); 
+	new Button({
+		text: "Press me",
+		type: "Default",
+		press: function() {
+			MessageToast.show("Button Cliked");
+		}
+	}).placeAt("content");
 });
